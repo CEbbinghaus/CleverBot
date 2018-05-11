a72d55078d22385d6f4b7c9f48c929688d3cf07b
Unknown <jecebbi@gmail.com>
2018-05-12 02:59:17 +1000

    Did a slight Rework

--- index.js
+++ index.js
@@ -6,12 +6,14 @@ var bot = new cleverbot(settings.capi.id,settings.capi.token);
 bot.setNick("Test")
 let dbot = new Client();
 bot.create(() => {});
-dbot.on("ready", () => console.log("Ready"))
-dbot.on("message", m => {
+dbot.on("ready", () => console.log("Ready"));
+dbot.on("message",async m => {
+  if(m.author.bot)return;
   if(m.isMentioned(m.guild.me)){
+    let mes = await m.reply("Processing...");
     let s = m.content.replace(/<@\d{18}>/g, "").trim();
     bot.ask(s, (err, response) => {
-      m.reply(response);
+      mes.edit(m.author + ", " + response);
   });
   }
 })
